bfdedc059c6c34802f759f1e04755d8637761163
BntngSantosa <bintangsantosa11@gmail.com>
2023-11-27 23:12:12 +0700

    update path index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.html", "./assets/js/**/*.js"],
+  content: ["./**/*.html", "./assets/js/**/*.js"],
   theme: {
     extend: {
       colors: {
