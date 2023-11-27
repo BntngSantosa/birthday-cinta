3998742cdc6d63cb6f2d3b603b357ff82ff6546c
BntngSantosa <bintangsantosa11@gmail.com>
2023-11-27 23:08:57 +0700

    Web khususon pun bojo

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.html", "./assets/js/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        "color-one": "#0A0A1F",
+        "color-two": "#BEBEBE",
+        "color-three": "#E4E5FF",
+        "color-four": "#3964D5",
+      },
+      fontFamily: {
+        "LeBelle": "La Belle Aurore",
+        "Poppins": "Poppins",
+      },
+      keyframes: {
+        "fadeLeft": {
+          '0%': { opacity: '0', transform: 'translateX(-100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        "fadeRight": {
+          '0%': { opacity: '0', transform: 'translateX(100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        "fadeLeft": 'fadeLeft 2s ease-out',
+        "fadeRight": 'fadeRight 2s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
+
