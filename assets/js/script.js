c0d611104a9814f672ffbc3752dbe31b3e11116a
BntngSantosa <bintangsantosa11@gmail.com>
2023-12-21 17:34:33 +0700

    update javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,20 @@ const audioPlayer = document.getElementById("audioPlayer");
 const btnAudioPlayer = document.querySelector("#btnAudioPlayer");
 
 btnAudioPlayer.addEventListener("click", () => {
-    btnAudioPlayer.classList.toggle("fa-circle-pause");
-    btnAudioPlayer.classList.toggle("fa-circle-play");
+  btnAudioPlayer.classList.toggle("fa-circle-pause");
+  btnAudioPlayer.classList.toggle("fa-circle-play");
 
-    btnAudioPlayer.classList.contains("fa-circle-pause") ? playSong() : stopSong();
+  // Memeriksa kelas ikon saat ini dan memanggil fungsi yang sesuai
+  if (btnAudioPlayer.classList.contains("fa-circle-play")) {
+    // Memastikan ada interaksi pengguna sebelum memainkan lagu
+    document.body.addEventListener( "click", function playHandler() {
+        playSong();
+        // Hapus event listener setelah pemutaran dimulai
+        document.body.removeEventListener("click", playHandler);
+    }, { once: true });
+  } else {
+    stopSong();
+  }
 });
 
 // Fungsi untuk memutar lagu
