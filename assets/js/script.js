03633c6e1eef000e0a0c08f6738da0a54ab0f737
BntngSantosa <bintangsantosa11@gmail.com>
2023-12-21 17:30:06 +0700

    update UI and added music

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Mendapatkan referensi ke elemen audio
+const audioPlayer = document.getElementById("audioPlayer");
+const btnAudioPlayer = document.querySelector("#btnAudioPlayer");
+
+btnAudioPlayer.addEventListener("click", () => {
+    btnAudioPlayer.classList.toggle("fa-circle-pause");
+    btnAudioPlayer.classList.toggle("fa-circle-play");
+
+    btnAudioPlayer.classList.contains("fa-circle-pause") ? playSong() : stopSong();
+});
+
+// Fungsi untuk memutar lagu
+function playSong() {
+  audioPlayer.play();
+}
+
+playSong();
+
+// Fungsi untuk menghentikan lagu
+function stopSong() {
+  audioPlayer.pause();
+  audioPlayer.currentTime = 0;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,22 @@ module.exports = {
         "color-four": "#3964D5",
       },
       fontFamily: {
-        "LeBelle": "La Belle Aurore",
-        "Poppins": "Poppins",
+        LeBelle: "La Belle Aurore",
+        Poppins: "Poppins",
       },
       keyframes: {
-        "fadeLeft": {
-          '0%': { opacity: '0', transform: 'translateX(-100%)' },
-          '100%': { opacity: '1', transform: 'translateX(0)' },
+        fadeLeft: {
+          "0%": { opacity: "0", transform: "translateX(-100%)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
         },
-        "fadeRight": {
-          '0%': { opacity: '0', transform: 'translateX(100%)' },
-          '100%': { opacity: '1', transform: 'translateX(0)' },
+        fadeRight: {
+          "0%": { opacity: "0", transform: "translateX(100%)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
         },
       },
       animation: {
-        "fadeLeft": 'fadeLeft 2s ease-out',
-        "fadeRight": 'fadeRight 2s ease-out',
+        fadeLeft: "fadeLeft 2s ease-out",
+        fadeRight: "fadeRight 2s ease-out",
       },
     },
   },
